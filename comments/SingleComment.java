4594ac6301d66f1ecd4aaea233450a0ff49ef15e
jdrkanchana <66084534+jdrkanchana@users.noreply.github.com>
2021-06-21 07:53:47 +0530

    Create SingleComment.java

---
+++ comments/SingleComment.java
@@ -0,0 +1,9 @@
+class SingleComment{
+public static void main(String[] args) {
+//x-number of students participate to lesson 1
+//y-number of students participate to lesson 2
+  int x=10, y=11,sum;
+  sum=x+y;
+  System.out.println(sum);
+}
+}
