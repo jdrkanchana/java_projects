e61042877588b98af2898127a28d18f63f2ae6fd
jdrkanchana <66084534+jdrkanchana@users.noreply.github.com>
2021-06-21 08:03:37 +0530

    Create SensibleNames.java

---
+++ variables/sensible_names/SensibleNames.java
@@ -0,0 +1,9 @@
+class SensibleNames{
+public static void main(String[] args) {
+  int no_of_students_lec1=10; 
+  int no_of_students_lec2=11;
+  int sum;
+  sum=no_of_students_lec1+no_of_students_lec2;
+  System.out.println( sum);
+}
+}
